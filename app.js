d72d3bb12dbc42f0360ca9a86716bcaf61ead678
dangolbeeker <dangolbeeker>
2022-04-02 13:37:31 -0600

    init

---
+++ app.js
@@ -0,0 +1,14 @@
+const { App } = require("@slack/bolt");
+require("dotenv").config();
+// Initializes your app with your bot token and signing secret
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
+
+(async () => {
+  const port = 3000
+  // Start your app
+  await app.start(process.env.PORT || port);
+  console.log(`⚡️ Slack Bolt app is running on port ${port}!`);
+})();
